9023a8ffc0f5cfc9d5b33f13f08e95651454b1dd
ultimate-tester <jordimueters@hotmail.com>
2015-06-23 10:24:31 +0200

    Added IntelliJ Automation
    Added gitignore, auto formatted source files, configured project to
    automatically create .min.js files.

--- mTab.js
+++ mTab.js
@@ -1,44 +1,44 @@
-(function($) {
-	$.fn.mTab = function(options) {
-		var settings = $.extend({
-			startTab: 0
-		}, options);
-		
-		var tabs = [];
-		
-		var $tabWrapper = $(this);
-		var $tabControlBar = $('<div class="mTabControlBar"></div>').prependTo($tabWrapper);
-		var $tabControlBarList = $('<ul></ul>').prependTo($tabControlBar);
-		
-		$tabWrapper.collapseAll = function() {
-			$tabWrapper.find('.mTab').hide();
-			$tabControlBarList.find('li').removeClass('mTabActive');
-		};
-		
-		$tabWrapper.activateTab = function(index) {
-			$tabWrapper.tabs[index].activateTab();
-		};
-		
-		$tabWrapper.find('.mTab').each(function(index, element) {
-			var $element = $(element);
-			var $tab = $('<li><a href="javascript://">' + $element.attr('data-mTabTitle') + '</a></li>').appendTo($tabControlBarList);
-			$element.activateTab = function() {
-				$tabWrapper.collapseAll();
-				$tab.addClass('mTabActive');
-				$element.show();
-			};
-			
-			$tab.find('a').click(function() { 
-				$element.activateTab();
-			});
-			
-			if(index == settings.startTab) {
-				$element.activateTab();
-			}
-			
-			$element.tabs[index] = $element;
-		});
-		
-		return $tabWrapper;
-	};
-} (jQuery));
+(function ($) {
+    $.fn.mTab = function (options) {
+        var settings = $.extend({
+            startTab: 0
+        }, options);
+
+        var $tabWrapper = $(this);
+        $tabWrapper.tabs = [];
+
+        var $tabControlBar = $('<div class="mTabControlBar"></div>').prependTo($tabWrapper);
+        var $tabControlBarList = $('<ul></ul>').prependTo($tabControlBar);
+
+        $tabWrapper.collapseAll = function () {
+            $tabWrapper.find('.mTab').hide();
+            $tabControlBarList.find('li').removeClass('mTabActive');
+        };
+
+        $tabWrapper.activateTab = function (index) {
+            $tabWrapper.tabs[index].activateTab();
+        };
+
+        $tabWrapper.find('.mTab').each(function (index, element) {
+            var $element = $(element);
+            var $tab = $('<li><a href="javascript://">' + $element.attr('data-mTabTitle') + '</a></li>').appendTo($tabControlBarList);
+            $element.activateTab = function () {
+                $tabWrapper.collapseAll();
+                $tab.addClass('mTabActive');
+                $element.show();
+            };
+
+            $tab.find('a').click(function () {
+                $element.activateTab();
+            });
+
+            if (index == settings.startTab) {
+                $element.activateTab();
+            }
+
+            $element.tabs[index] = $element;
+        });
+
+        return $tabWrapper;
+    };
+}(jQuery));
